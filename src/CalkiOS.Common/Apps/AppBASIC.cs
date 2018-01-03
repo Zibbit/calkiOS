298fcbff269c2773dc270ce672455f7ceec80d70
Gokul Ravindran <neohex262@gmail.com>
2018-01-03 23:51:44 +0530

    Create AppBASIC.cs

---
+++ src/CalkiOS.Common/Apps/AppBASIC.cs
@@ -0,0 +1,9 @@
+// This project licenses this file to you under the Unlicense.
+// See the LICENSE file in the project root for more information.
+
+namespace PupOS.Common.Apps
+{
+    class AppBASIC
+    {
+    }
+}
