c85150b506ad1154f875431b3f38236fd06f83d0
Gokul Ravindran <neohex262@gmail.com>
2018-01-03 23:52:08 +0530

    Update AppBASIC.cs

--- src/CalkiOS.Common/Apps/AppBASIC.cs
+++ src/CalkiOS.Common/Apps/AppBASIC.cs
@@ -1,7 +1,7 @@
 // This project licenses this file to you under the Unlicense.
 // See the LICENSE file in the project root for more information.
 
-namespace PupOS.Common.Apps
+namespace CalkiOS.Common.Apps
 {
     class AppBASIC
     {
